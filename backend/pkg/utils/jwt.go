03877b30609a38cdf492512fa2a8b3a08122eba9
afdalrafi3107 <afdalrafi990@gmail.com>
2025-11-15 20:18:47 +0700

    fix: jwt token not exist

--- backend/pkg/utils/jwt.go
+++ backend/pkg/utils/jwt.go
@@ -1,18 +1,19 @@
 package utils
 
 import (
-	"os"
+	"rental-book/internal/config"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
 
 func GenerateToken(id uint) (string, error) {
+	config.EnvLoad()
 	claims := jwt.MapClaims{
 		"user_id":id,
 		"exp": time.Now().Add(24 * time.Hour).Unix(),
 	}
-	token:=jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token:=jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	return token.SignedString([]byte("ikanamasnd"))
 }
